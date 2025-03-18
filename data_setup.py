898fc031f6168ee09f7c9d6b47c5beb05e9960bc
guya lee <daniellee6925@gmail.com>
2025-03-17 21:29:42 -0700

    add data split function

--- data_setup.py
+++ data_setup.py
@@ -1,6 +1,8 @@
 """This function creates PyTorch DataLoaders for iamge classification data"""
 
 import os
+import torchvision
+import torch
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 
@@ -51,3 +53,34 @@ def create_dataloaders(
     )
 
     return train_dataloader, test_dataloader, class_names
+
+
+def split_dataset(
+    dataset: torchvision.datasets, split_size: float = 0.2, seed: int = 42
+):
+    """Randomly splits a given dataset into two proportions based on split_size and seed.
+
+    Args:
+        dataset (torchvision.datasets): A PyTorch Dataset, typically one from torchvision.datasets.
+        split_size (float, optional): How much of the dataset should be the validation set? Defaults to 0.2.
+        seed (int, optional): Seed for random generator. Defaults to 42.
+
+    Returns:
+        tuple: (random_split_1, random_split_2) where random_split_1 is of size split_size*len(dataset) and
+            random_split_2 is of size (1-split_size)*len(dataset).
+    """
+
+    # create split length based on original dataset size
+    size_1 = int(len(dataset) * split_size)  # desired length
+    size_2 = len(dataset) - size_1  # remaining length
+
+    print(
+        f"[INFO] Splitting dataset of length {len(dataset)} into splits of size: {size_1} ({int(split_size * 100)}%), {size_2} ({int((1 - split_size) * 100)}%)"
+    )
+
+    # create splits given random seed
+    set_1, set_2 = torch.utils.data.random_split(
+        dataset, lengths=[size_1, size_2], generator=torch.manual_seed(seed)
+    )
+
+    return set_1, set_2
